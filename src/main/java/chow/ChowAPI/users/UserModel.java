979daa4bdde909d0bf31b540779ded4dd0e656b6
Victoria Sawchuk <vtsawchuk@gmail.com>
2018-01-12 11:16:54 -0800

    Enforce uniqueness on users, prevent user creation if user already exists

--- src/main/java/chow/ChowAPI/users/UserModel.java
+++ src/main/java/chow/ChowAPI/users/UserModel.java
@@ -1,13 +1,12 @@
 package chow.ChowAPI.users;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.stereotype.Component;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-//import java.util.ArrayList;
-//import java.util.Iterator;
-//import java.util.List;
+import java.util.List;
 
 @Repository
 public interface UserModel extends JpaRepository<User, Long> {
+    List<User> findByEmailAndGoogleId(String email, String google_id);
 }
--- src/main/java/chow/ChowAPI/users/UsersController.java
+++ src/main/java/chow/ChowAPI/users/UsersController.java
@@ -1,6 +1,10 @@
 package chow.ChowAPI.users;
 
 import java.net.URI;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -8,8 +12,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
-import javax.persistence.EntityManager;
 
 @RestController
 public class UsersController {
@@ -33,11 +35,24 @@ public class UsersController {
     // TODO: require auth token from google
     @PostMapping(path = "/users")
     public ResponseEntity<User> createUser(@RequestBody User user) {
-        User savedUser = model.save(user);
-        URI location = ServletUriComponentsBuilder
-                .fromCurrentRequest().path("/{id}")
-                .buildAndExpand(savedUser.getId()).toUri();
-        ResponseEntity response = ResponseEntity.created(location).body(savedUser);
-        return response;
+        User savedUser;
+        try {
+            List<User> matchingUsers = model.findByEmailAndGoogleId(user.getEmail(), user.getGoogleId());
+            if (matchingUsers.size() == 1) {
+                User existingUser = matchingUsers.get(0);
+                URI location = ServletUriComponentsBuilder
+                        .fromCurrentRequest().path("/{id}")
+                        .buildAndExpand(existingUser.getId()).toUri();
+                return ResponseEntity.created(location).body(existingUser);
+            } else {
+                savedUser = model.save(user);
+                URI location = ServletUriComponentsBuilder
+                        .fromCurrentRequest().path("/{id}")
+                        .buildAndExpand(savedUser.getId()).toUri();
+                return ResponseEntity.created(location).body(savedUser);
+            }
+        } catch (Exception ex) {
+            return ResponseEntity.badRequest().build();
+        }
     }
 }
